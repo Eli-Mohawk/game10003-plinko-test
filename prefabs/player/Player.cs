b6c65225993d67115a5b62fa99d5d1e42f1543b2
Eli-Mohawk <eli.poag@mohawkcollege.ca>
2025-12-01 13:52:38 -0500

    video 1

--- prefabs/player/Player.cs
+++ prefabs/player/Player.cs
@@ -6,12 +6,19 @@ public partial class Player : RigidBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		Freeze = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (Freeze)
+		{
+			Vector2 newPos = Position;
+			newPos.X = GetViewport().GetMousePosition().X;
+			Position = newPos;
 
+			if (Input.IsActionJustPressed("drop_player")) Freeze = false;
+		}
 	}
 }
